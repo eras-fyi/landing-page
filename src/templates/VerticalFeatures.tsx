5260b80734e687fb9a5f025c168fe7eeb54bf329
Edmond Tam <edmondtam2@gmail.com>
2023-08-03 16:24:39 +0100

    Update image to use sample plan and text points

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -26,8 +26,8 @@ const VerticalFeatures = () => {
         <VerticalFeatureRow
           title="Make an Action Plan"
           description="Use our AI-tool to identify a goal you have - it doesn’t even have to be financial - and get inspired by links in each step."
-          image="/assets/images/feature.svg"
-          imageAlt="First feature alt text"
+          image="/assets/images/sample-plan.svg"
+          imageAlt="Sample plan for users"
           buttonText="Get Started"
           buttonLink="https://app.eras.fyi"
           buttonPrimary
@@ -36,7 +36,7 @@ const VerticalFeatures = () => {
           title="Follow Our Guidance"
           description="For £5 a week - the price of a sandwich - we’ll do everything to ensure you follow through on your plan. We'll call you, set calendar invites, message you on your favourite platforms. What method works best for you? Don’t be shy - let us know!"
           image="/assets/images/feature2.svg"
-          imageAlt="Second feature alt text"
+          imageAlt="Getting guidance to your goal"
           reverse
           buttonText="Join Waitlist"
           buttonLink="/"
@@ -44,9 +44,9 @@ const VerticalFeatures = () => {
         />
         <VerticalFeatureRow
           title="Automate It"
-          description="When you’ve achieved your goals, we’ll automate the maintenance of it. No longer will you have to worry about this goal - let’s move on to the next!"
+          description="When you’ve achieved your goals, we’ll automate what you need to. No longer will you have to worry about this goal - let’s move on to the next!"
           image="/assets/images/feature3.svg"
-          imageAlt="Third feature alt text"
+          imageAlt="Automating your goals"
           buttonText="Join Waitlist"
           buttonLink="/"
           buttonOnClick={toggleModalVisibility}
